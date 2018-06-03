986de316f8dfbc0d0c6db95a2c8ac5f66310c78f
Antoine Foggs <fireheartfoggs@gmail.com>
2018-06-03 18:48:12 -0500

    Got profile to display properly.

--- week-02/index.js
+++ week-02/index.js
@@ -9,13 +9,21 @@ const options = {
   // TODO: Create an object literal to hit your account on the GitHub API,
   // and pass any other information the API requires (you should not have
   // to authenticate for this, however)
+  uri: 'https://api.github.com/users/afoggs',
+  headers: {
+      'User-Agent': 'Request-Promise'
+  },
+  json: true // Automatically parses the JSON string in the response
 };
 
+
 req(options)
-  .then(
+  .then(function(profile){
     // TODO: Handle the returned JSON data and write it to a file called
     // `response.json` in your `week-two/` directory
-  )
+    console.log(profile);
+  })
+
   .catch(function(err) {
     console.log(err);
   });
