dba4c1e06ff01e5a309d1da9eb94f807c9975eb0
Antoine Foggs <fireheartfoggs@gmail.com>
2018-06-14 17:40:37 -0500

    Fixed js issue.

--- week-03/github/routes/index.js
+++ week-03/github/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request-promise-native ');
+
+const options = {
+  uri: 'https://api.github.com/users/afoggs'
+  headers: {
+    'User-Agent': 'Request-Promise'
+  },
+  json: true;
+}
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Week 3' });
 });
 
 module.exports = router;
--- week-03/github/routes/users.js
+++ week-03/github/routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+const req = require('request-promise-native');
+
+const options = {
+  // TODO: Create an object literal to hit your account on the GitHub API,
+  // and pass any other information the API requires (you should not have
+  // to authenticate for this, however)
+  uri: 'https://api.github.com/users/afoggs',
+  headers: {
+      'User-Agent': 'Request-Promise'
+  },
+  json: true // Automatically parses the JSON string in the response
+};
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  req(options)
+    .then( function(profile){
+      // TODO: Handle the returned JSON data and write it to a file called
+      // `response.json` in your `week-two/` directory
+      //display data for verification.
+      console.log(profile);
+ res.render('index', { title: 'Github', name: results.name, avatar: results.avatar_url, repos: results.public_repos, updated_at: Date(results.updated_at) });
+});
 });
 
 module.exports = router;
