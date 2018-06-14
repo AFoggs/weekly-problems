35b93b467e89fc6209ccf80e8a13dd88e6aa5845
Antoine Foggs <fireheartfoggs@gmail.com>
2018-06-14 18:20:13 -0500

    Redid js

--- week-03/github/routes/index.js
+++ week-03/github/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const req = require('request-promise-native ');
+
+const options = {
+  uri: 'https://api.github.com/users/afoggs',
+  headers: {
+    'User-Agent': 'Request-Promise'
+  },
+  json: true;
+}
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  req(options).then(function(results) {
+  console.log(results.name);
+  res.render('index', { title: 'Week 3: PUG & Github', name: result.name, profile: results.profile });
+});
 });
 
 module.exports = router;
