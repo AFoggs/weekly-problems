6a832e808f9eae71de8de6dd0276f425a2bdc0a8
Antoine Foggs <fireheartfoggs@gmail.com>
2018-06-14 18:01:12 -0500

    Reinitialized express.

--- week-03/github/routes/index.js
+++ week-03/github/routes/index.js
@@ -1,19 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var request = require('request-promise-native ');
-
-const options = {
-  uri: 'https://api.github.com/users/afoggs'
-  headers: {
-    'User-Agent': 'Request-Promise'
-  },
-  json: true;
-}
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Week 3' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
